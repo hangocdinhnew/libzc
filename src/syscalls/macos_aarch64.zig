3f6ae645fa504f48c828920307fd46066d965831
hangocdinhnew <dinhndrwnew@gmail.com>
2026-02-10 23:09:31 +0700

    Fix issue for MacOS.

--- src/syscalls/macos_aarch64.zig
+++ src/syscalls/macos_aarch64.zig
@@ -1,7 +1,8 @@
 pub inline fn _exit(status: i32) noreturn {
     asm volatile (
-        \\ mov x16, #0x2000001
-        \\ svc #0x80
+        \\movz x16, #0x0001
+        \\movk x16, #0x2000, lsl #16
+        \\ svc #0
         :
         : [status] "{x0}" (status),
         : .{
@@ -15,8 +16,9 @@ pub inline fn _exit(status: i32) noreturn {
 
 pub inline fn write(fd: i32, buf: [*c]const u8, len: usize) isize {
     return asm volatile (
-        \\ mov x16, #0x2000004
-        \\ svc #0x80
+        \\movz x16, #0x0004
+        \\movk x16, #0x2000, lsl #16
+        \\svc #0
         : [ret] "={x0}" (-> isize),
         : [fd] "{x0}" (fd),
           [buf] "{x1}" (buf),
